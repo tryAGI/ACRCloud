bc406d8623ce416713decddc2b7882922176e746
Konstantin S. <havendv@gmail.com>
2026-08-27 17:46:31 +0400

    feat: Updated OpenAPI spec (#17)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ACRCloud/Generated/ACRCloud.JsonSerializerContextTypes.g.cs
+++ src/libs/ACRCloud/Generated/ACRCloud.JsonSerializerContextTypes.g.cs
@@ -6,96 +6,96 @@
 namespace ACRCloud
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public sealed partial class JsonSerializerContextTypes
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.Dictionary<string, string>? StringStringDictionary { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.Dictionary<string, object>? StringObjectDictionary { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Text.Json.JsonElement? JsonElement { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.IdentifyRequest? Type0 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public byte[]? Type1 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public string? Type2 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public long? Type3 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.IdentifyResponse? Type4 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.Status? Type5 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.Metadata? Type6 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public int? Type7 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public double? Type8 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.IList<global::ACRCloud.MusicMetadata>? Type9 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.MusicMetadata? Type10 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::ACRCloud.LocalizedName? Type11 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.IList<global::ACRCloud.LocalizedName>? Type12 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public object? Type13 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.IList<object>? Type14 { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.List<global::ACRCloud.MusicMetadata>? ListType0 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.List<global::ACRCloud.LocalizedName>? ListType1 { get; set; }
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public global::System.Collections.Generic.List<object>? ListType2 { get; set; }
     }
--- src/libs/ACRCloud/Generated/ACRCloud.Models.MusicMetadata.g.cs
+++ src/libs/ACRCloud/Generated/ACRCloud.Models.MusicMetadata.g.cs
@@ -4,90 +4,90 @@
 namespace ACRCloud
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public sealed partial class MusicMetadata
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("acrid")]
         public string? Acrid { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("title")]
         public string? Title { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("album")]
         public global::ACRCloud.LocalizedName? Album { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("artists")]
         public global::System.Collections.Generic.IList<global::ACRCloud.LocalizedName>? Artists { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("release_date")]
         public string? ReleaseDate { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("duration_ms")]
         public int? DurationMs { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("score")]
         public int? Score { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("external_ids")]
         public object? ExternalIds { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("external_metadata")]
         public object? ExternalMetadata { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("play_offset_ms")]
         public int? PlayOffsetMs { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("db_begin_time_offset_ms")]
         public int? DbBeginTimeOffsetMs { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("db_end_time_offset_ms")]
         public int? DbEndTimeOffsetMs { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("sample_begin_time_offset_ms")]
         public int? SampleBeginTimeOffsetMs { get; set; }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("sample_end_time_offset_ms")]
         public int? SampleEndTimeOffsetMs { get; set; }
